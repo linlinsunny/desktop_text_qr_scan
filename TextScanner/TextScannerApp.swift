5c7ba42783c007ee4c709fd50ec1df4245fdbe24
littlebitch <sunnydodo@msn.com>
2025-07-18 14:07:08 +0800

    Initial Commit

---
+++ TextScanner/TextScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextScannerApp.swift
+//  TextScanner
+//
+//  Created by sunxiaojian on 2025/7/18.
+//
+
+import SwiftUI
+
+@main
+struct TextScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TextScannerTests/TextScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TextScannerTests.swift
+//  TextScannerTests
+//
+//  Created by sunxiaojian on 2025/7/18.
+//
+
+import Testing
+@testable import TextScanner
+
+struct TextScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
